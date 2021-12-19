fa49f07094ca5b1f0a15cefbfbb87546f7519090
mskwak <kase1@naver.com>
2021-12-19 18:47:41 +0900

    first commit

---
+++ app/src/main/java/com/mskwak/toy_todo/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mskwak.toy_todo.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mskwak.toy_todo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
