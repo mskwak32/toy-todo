965629101c92d2c9c0f1f0f1990bfa16ddf55e20
mskwak <kase1@naver.com>
2022-01-26 16:46:33 +0900

    room 삭제

--- app/src/main/java/com/mskwak/toy_todo/model/Task.kt
+++ app/src/main/java/com/mskwak/toy_todo/model/Task.kt
@@ -1,11 +1,7 @@
 package com.mskwak.toy_todo.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "taskTable")
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val id: Long = 0,
     var title: String = "",
     var memo: String = "",
     var isCompleted: Boolean = false
